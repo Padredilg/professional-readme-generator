fbc158384ccd435b71360405bb068ff10a1e9223
Luiz P <padredilg@knights.ucf.edu>
2021-08-22 14:46:42 -0400

    created function to render license on readme body

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,19 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  console.log("Badge Generated");
+  console.log("Badge Generated for " + license);
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  console.log("Link Generated");
+  console.log("Link Generated for " + license);
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  console.log("License Section Generated");
+  console.log("License Section Generated for " + license);
 }
 
 function generateTableOfContents(confirm, data){
@@ -70,7 +70,7 @@ function generateUsage(confirm, description, data){
   if(confirm){
     let displayImage = '';
     if(data.confirmUsageSS){
-      displayImage = '<img width="1433" alt="decide eat website" src="' + data.usageImagePath + '"></img>';
+      displayImage = '<img alt="screenshot from website" src="' + data.usageImagePath + '"></img>';
     }
     else{
       displayImage = '';
@@ -86,7 +86,21 @@ ${displayImage}
   }
 }
 
-// TODO: Create a function to generate markdown for README
+function generateLicense(confirm, license){
+  if(confirm){
+    renderLicenseBadge(license);
+    renderLicenseLink(license);
+    renderLicenseSection(license);
+
+    return `## License
+    ${license}
+    `
+  }
+  else{
+    return '';
+  }
+}
+
 function generateMarkdown(data) {
 
   return `# ${data.title}
@@ -97,11 +111,9 @@ ${data.description}
 
 ${generateInstallation(data.confirmInstallation, data.installationDescription)}
 ${generateUsage(data.confirmUsage, data.usageDescription, data)}
-
+${generateLicense(data.confirmLicense, data.license)}
 `;
 }
 //include remaining generated info in template
 
 module.exports = generateMarkdown;
-
-//So I guess I need to export/require this file passing the readMe data to it
